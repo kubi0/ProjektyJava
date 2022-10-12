2af75508f3e7b0bf32f33f5c7105d1b02f892935
kubi0 <jwyszyn2@stu.vistula.edu.pl>
2022-10-12 20:21:38 +0200

    Initial commit

---
+++ src/pl/vistula/helloworldnazwisko/HelloWorldNazwisko.java
@@ -0,0 +1,9 @@
+package pl.vistula.helloworldnazwisko;
+
+public class HelloWorldNazwisko {
+    public static void main(String[] args) {
+        System.out.println("„Hello Vistula Nazwisko");
+        System.out.println("Witam");
+        System.out.println("Hello World");
+    }
+}
